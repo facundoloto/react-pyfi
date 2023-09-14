26a425d7337f99cfc5e765cf76230ce44425355a
ema <lotofacundo6@gmail.com>
2023-09-14 16:19:14 -0300

    fix btn edit profile

--- src/components/profile/Header.jsx
+++ src/components/profile/Header.jsx
@@ -1,5 +1,6 @@
 import { useState, lazy } from "react";
 import { getUserById } from "../../api/fetchApi";
+import { useAuthStore } from "../../store/authStore";
 import { useQuery } from "@tanstack/react-query";
 const FormProfile = lazy(() => import("./FormProfile"));
 import styleHeader from './profile.module.css';
@@ -7,7 +8,9 @@ import "./profile.module.css";
 
 export default function Header({ id }) {
   const [showModal, setShowModal] = useState(false);
+  const idUser = useAuthStore((state) => state.idUser);
 
+  const isActive = idUser == id ? true : false;
   const { isLoading, data: user, isError, error } = useQuery({
     queryKey: ['user', id],
     queryFn: () => getUserById(id),
@@ -36,7 +39,10 @@ export default function Header({ id }) {
         </div>
         <div className={styleHeader.infoUser}>
           <h1 className={styleHeader.profileUserName}>{user.result.name}</h1>
-          <button className={styleHeader.profileBtn} onClick={openModal}>Edit Profile</button>
+          {
+            isActive && (
+              <button className={styleHeader.profileBtn} onClick={openModal}>Edit Profile</button>
+            )}
         </div>
       </div>
       <FormProfile show={showModal} handleClose={closeModal} />
